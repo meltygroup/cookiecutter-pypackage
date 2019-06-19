1b62b6d200d10cebbc3435c07a0ac57e187ea3e0
Julien Palard <julien@palard.fr>
2019-06-19 11:25:59 +0200

    Build the venv also for libraries.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,18 +9,24 @@ def run(command):
     check_output(command.split())
 
 
+os.chdir(PROJECT_DIRECTORY)
+
+run("git init")
+run("python3 -m venv venv")
+run("venv/bin/python -m pip install -U pip")
+run("venv/bin/python -m pip install pip-tools")
+
 if "{{ cookiecutter.type }}" == "application":
+    # An app uses a requirements.txt but no setup.py
     os.remove("setup.py")
-    os.chdir(PROJECT_DIRECTORY)
-    run("git init")
-    run("python3 -m venv venv")
-    run("venv/bin/python -m pip install -U pip")
-    run("venv/bin/python -m pip install pip-tools")
     run("venv/bin/pip-compile requirements.in")
-    run("venv/bin/pip-compile requirements-dev.in")
-    run("venv/bin/python -m pip install -r requirements-dev.txt")
-    run("git add .")
-    run("git commit -m Initial")
 else:
+    # A library uses a setup.py with requirements in it.
     os.remove("requirements.in")
-    os.remove("requirements-dev.in")
+    run("venv/bin/pip-compile setup.py")
+
+
+run("venv/bin/pip-compile requirements-dev.in")
+run("venv/bin/python -m pip install -r requirements-dev.txt")
+run("git add .")
+run("git commit -m Initial")
