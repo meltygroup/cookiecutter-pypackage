e74dcfd51405e05a088f68bb15ae50cf6f2ef984
Julien Palard <julien@palard.fr>
2019-06-19 10:36:32 +0200

    More verbose construction of the venv, git init, requirements...

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,17 +3,25 @@ from subprocess import check_output
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
+
+def run(command):
+    print(command)
+    check_output(command.split())
+
+
 if "{{ cookiecutter.type }}" == "application":
     os.remove("setup.py")
     os.chdir(PROJECT_DIRECTORY)
-    check_output(["git", "init"])
-    check_output(["python3", "-m", "venv", "venv"])
-    check_output(["venv/bin/python", "-m", "pip", "install", "-U", "pip"])
-    check_output(["venv/bin/python", "-m", "pip", "install", "pip-tools"])
-    check_output(["venv/bin/pip-compile", "requirements.in"])
-    check_output(["venv/bin/pip-compile", "requirements-dev.in"])
-    check_output(["git", "add", "."])
-    check_output(["git", "commit", "-m", "Initial commit."])
+    run("git init")
+    run("python3 -m venv venv")
+    run("venv/bin/python -m pip install -U pip")
+    run("venv/bin/python -m pip install pip-tools")
+    run("venv/bin/pip-compile requirements.in")
+    run("venv/bin/pip-compile requirements-dev.in")
+    run("venv/bin/python -m pip install -r requirements-dev.txt")
+    run("venv/bin/python -m pip install -e .")
+    run("git add .")
+    run("git commit -m Initial")
 else:
     os.remove("requirements.in")
     os.remove("requirements-dev.in")
