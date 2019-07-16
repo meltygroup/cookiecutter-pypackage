bbafff793b756f7c20e3e42e79d0e90cd3132e29
Julien Palard <julien@palard.fr>
2019-07-16 14:57:56 +0200

    Simplify by moving the requirements.txt responsibility to the releaser, not the maintainer.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -14,20 +14,5 @@ os.chdir(PROJECT_DIRECTORY)
 run("git init")
 run("python3 -m venv venv")
 run("venv/bin/python -m pip install -U pip")
-run("venv/bin/python -m pip install pip-tools")
-
-if "{{ cookiecutter.type }}" == "application":
-    # An app uses a requirements.txt but no setup.py
-    os.remove("setup.py")
-    run("venv/bin/pip-compile requirements.in")
-else:
-    # A library uses a setup.py with requirements in it.
-    os.remove("requirements.in")
-    os.remove("{{ cookiecutter.project_slug }}/__main__.py")
-    run("venv/bin/pip-compile setup.py")
-
-
-run("venv/bin/pip-compile requirements-dev.in")
-run("venv/bin/python -m pip install -r requirements-dev.txt")
 run("git add .")
 run("git commit -m Initial")
